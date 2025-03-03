23c93868d5a56c3edbd8717aeefacf9934e4c455
Jonas <jonashooncantey@gmail.com>
2025-03-02 20:47:56 -0500

    Task 2 - Created Borrower Class

--- cc_11.js
+++ cc_11.js
@@ -27,3 +27,33 @@ console.log(book1.getDetails());
 
 book1.updateCopies(-1);     //test case 2, saying that someone borrowed a book
 console.log(book1.getDetails());
+
+//Task 2 - Creating a Borrower Class
+console.log("Task 2 - Creating a Borrower Class")
+
+class borrower {        //declare new class borrower
+    constructor(name, borrowerId) { //using the properties name and borrowerId
+        this.name = name;           //initializing properties
+        this.borrowerId = borrowerId;
+        this.borrowedBooks = [];
+    }
+    borrowBook(bookTitle) { //basically a function that pushes a new borrowed book to the array
+        this.borrowedBooks.push(bookTitle);
+    }
+
+    returnBook(bookTitle) { //method that looks for a book in the borrowed book array, if its found then
+        const index = this.borrowedBooks.indexOf(bookTitle);    //it will remove it from the array
+        if (index !== -1) {     //if not found it will log book not found
+            this.borrowedBooks.splice(index, 1);
+        } else {
+            console.log(`Book "${bookTitle}" not found in borrowed books.`);
+        }
+    }
+}
+
+const borrower1 = new borrower("Alice Johnson", 201);   //test case 1 borrowing book
+borrower1.borrowBook("The Great Gatsby");
+console.log(borrower1.borrowedBooks);
+
+borrower1.returnBook("The Great Gatsby");   //test case 2 returning book
+console.log(borrower1.borrowedBooks);
